4bf214ca7b5a016118692f20d668d1afef6702a8
Philippe Leite <philippeleite@192.168.68.104>
2025-06-23 23:29:52 -0300

    Main function fix3

--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,7 @@ ffi = FFI()
 ffi.cdef('''
 void read_memory(char* buffer, int length, intptr_t address);
  ''')
-C = ffi.dlopen('./zblocks.so')
+C = ffi.dlopen('src/zblocks.so')
 
 def read_memory(buffer: bytearray, size: int, address: int):
     C.read_memory(ffi.from_buffer(buffer), size, address)

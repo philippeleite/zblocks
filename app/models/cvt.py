e7428b2ee77ed2aae31c77e19a34eec76736fb1b
Philippe Leite <philippeleite@192.168.68.104>
2025-07-02 19:32:34 -0300

    CVT test 2

--- app/models/cvt.py
+++ app/models/cvt.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 
 zblocks_list.append('cvt')
 
-cvt_pattern= Struct(
+cvt_pattern = Struct(
     ">"
     "4s"  # CVTTCBP
     "4s"  # CVT0EF00
@@ -26,15 +26,12 @@ cvt_pattern= Struct(
     "4s"  # CVTZDTAB
     "4s"  # CVTXITP
     "4s"  # CVT0EF01
-    "2s"  # CVTVSS
-    "2s"  # CVTVPSM
+    "4s"  # CVTVPRM
     "2s"  # CVTEXIT
     "2s"  # CVTBRET
     "4s"  # CVTSVDCB
     "4s"  # CVTTPC
-    "s"   # CVTFLGC0
-    "s"   # CVTFLGC1
-    "2s"  # CVTICPID
+    "4s"  # CVTFLGCS
     "4s"  # CVTCVT
     "4s"  # CVTCUCB
 )
@@ -59,50 +56,49 @@ cvt_field_names = (
     "CVTZDTAB",
     "CVTXITP",
     "CVT0EF01",
-    "CVTVSS",
-    "CVTVPSM",
+    "CVTVPRM",
     "CVTEXIT",
     "CVTBRET",
     "CVTSVDCB",
     "CVTTPC",
-    "CVTFLGC0",
-    "CVTFLGC1",
-    "CVTICPID",
+    "CVTFLGCS",
     "CVTCVT",
     "CVTCUCB",
 )
 
 cvt_offset_length = (
-    info(0,4),      # CVTTCBP
-    info(4,4),      # CVT0EF00
-    info(8,4),      # CVTLINK
-    info(12,4),     # CVTAUSCB
-    info(16,4),     # CVTBUF
-    info(20,4),     # CVTXAPG
-    info(24,4),     # CVT0VL00
-    info(28,4),     # CVTPCNVT
-    info(32,4),     # CVTPRLTV
-    info(36,4),     # CVTLLCB
-    info(40,4),     # CVTLLTRM
-    info(44,4),     # CVTXTLER
-    info(48,4),     # CVTSYSAD
-    info(52,4),     # CVTBTERM
-    info(56,4),     # CVTDATE
-    info(60,4),     # CVTMSLT
-    info(64,4),     # CVTZDTAB
-    info(68,4),     # CVTXITP
-    info(72,4),     # CVT0EF01
-    info(76,4),     # CVTVPRM
-    info(80,4),     # CVTEXIT
-    info(84,4),     # CVTSVDCB
-    info(88,4),     # CVTTPC
-    info(92,4),     # CVTFLGCS
-    info(96,4),     # CVTCVT
-    info(100,4),    # CVTCUCB
+    info(0, 4),  # CVTTCBP
+    info(4, 4),  # CVT0EF00
+    info(8, 4),  # CVTLINK
+    info(12, 4),  # CVTAUSCB
+    info(16, 4),  # CVTBUF
+    info(20, 4),  # CVTXAPG
+    info(24, 4),  # CVT0VL00
+    info(28, 4),  # CVTPCNVT
+    info(32, 4),  # CVTPRLTV
+    info(36, 4),  # CVTLLCB
+    info(40, 4),  # CVTLLTRM
+    info(44, 4),  # CVTXTLER
+    info(48, 4),  # CVTSYSAD
+    info(52, 4),  # CVTBTERM
+    info(56, 4),  # CVTDATE
+    info(60, 4),  # CVTMSLT
+    info(64, 4),  # CVTZDTAB
+    info(68, 4),  # CVTXITP
+    info(72, 4),  # CVT0EF01
+    info(76, 4),  # CVTVPRM
+    info(80, 2),  # CVTEXIT
+    info(82, 2),  # CVTBRET
+    info(84, 4),  # CVTSVDCB
+    info(88, 4),  # CVTTPC
+    info(92, 4),  # CVTFLGCS
+    info(96, 4),  # CVTCVT
+    info(100, 4),  # CVTCUCB
 )
 
 cvt_fields = namedtuple("cvt", cvt_field_names)
 cvt_info = cvt_fields._make(cvt_offset_length)
+
 
 def get_cvt_address() -> int:
     cvt_address_buffer = bytearray(4)

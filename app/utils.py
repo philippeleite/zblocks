e7b81d7d094e13ab5a8808a369e9d5ddbfac0394
Philippe Leite <philippeleite@192.168.68.104>
2025-07-14 19:08:29 -0300

    PC routine call

--- app/utils.py
+++ app/utils.py
@@ -2,3 +2,6 @@ from zutils import lib, ffi
 
 def read_memory(buffer: bytearray, size: int, address: int):
     lib.read_memory(ffi.from_buffer(buffer), size, address)
+
+def read_memory_auth(buffer: bytearray, size: int, address: int):
+    lib.read_memory_auth(ffi.from_buffer(buffer), size, address)

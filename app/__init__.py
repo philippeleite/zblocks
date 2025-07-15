d1fcd295148be1e89d6115d91b2ed84886a102ed
Philippe Leite <philippeleite@192.168.68.104>
2025-07-15 13:54:31 -0300

    OUCB test 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from dotenv import load_dotenv
-load_dotenv()
+
 
 app = Flask(__name__)
 
--- app/models/base.py
+++ app/models/base.py
@@ -7,3 +7,18 @@ zblocks_list = []
 
 zblocks_graph = nx.Graph()
 
+zblocks_graph.add_nodes_from(zblocks_list)
+zblocks_graph.add_edge("PSA", "CVT", path="FLCCVT")
+zblocks_graph.add_edge("PSA", "PCCA", path="PSAPCCAV")
+zblocks_graph.add_edge("PSA", "LCCA", path="PSALCCAV")
+zblocks_graph.add_edge("PSA", "ASCB", path="PSAAOLD")
+zblocks_graph.add_edge("PSA", "TCB", path="PSATOLD")
+zblocks_graph.add_edge("CVT", "ASVT", path="CVTASVT")
+zblocks_graph.add_edge("CVT", "ECVT", path="CVTECVT")
+zblocks_graph.add_edge("ASCB", "ASSB", path="ASCBASSB")
+zblocks_graph.add_edge("ASCB", "ASXB", path="ASCBASXB")
+zblocks_graph.add_edge("ASCB", "OUCB", path="ASCBOUCB")
+zblocks_graph.add_edge("ASSB", "JSAB", path="ASSBJSAB")
+zblocks_graph.add_edge("ASXB", "ACEE", path="ASXBSENV")
+zblocks_graph.add_edge("TCB", "JSCB", path="TCBJSCB")
+zblocks_graph.add_edge("TCB", "ACEE", path="TCBSENV")

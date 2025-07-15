e51a13cb37fc4ae27d0ea8c9810b7c1f064b134d
Philippe Leite <philippeleite@192.168.68.104>
2025-07-14 22:56:59 -0300

    PC routine call

--- app/build.py
+++ app/build.py
@@ -26,34 +26,33 @@ void read_memory_auth(char *buffer,
     const char *name = "ZBLOCKS         ";
     unsigned char token[16];   
     unsigned int wk_rc;  
-    unsigned int plist[4];
+    unsigned long plist[4];
     unsigned int *lx; 
-    plist[0] = (unsigned int)(void * __ptr32) &system_level;
-    plist[1] = (unsigned int)(void * __ptr32) name;
-    plist[2] = (unsigned int)(void * __ptr32) token;
-    plist[3] = (unsigned int)(void * __ptr32) &wk_rc;  
-    __asm(" SAM31\n"
-          " L 15,x'10'\n"
+    plist[0] = (unsigned long)(void *) &system_level;
+    plist[1] = (unsigned long)(void *) name;
+    plist[2] = (unsigned long)(void *) token;
+    plist[3] = (unsigned long)(void *) &wk_rc;  
+    __asm(" L 15,x'10'\n"
           " L 15,x'220'(15,0)\n"
           " L 15,x'14'(15,0)\n"
-          " L 15,x'08'(15,0)\n"
-          " BAS 14,15\n"
-          " SAM64\n"
+          " L 15,x'80'(15,0)\n"
+          " BASR 14,15\n"
           : 
           : "NR:r1"(plist)
           : "r1", "r14", "r15");
     
+    unsigned int plist31[3];
     if (wk_rc == 0) {
         lx = (unsigned int *)(void *)token;
-        plist[0] = (unsigned int)(void * __ptr32)buffer;
-        plist[1] = (unsigned int)(void * __ptr32)&length;
-        plist[2] = (unsigned int)(void * __ptr32)&address; 
+        plist31[0] = (unsigned int)(void * __ptr32)buffer;
+        plist31[1] = (unsigned int)(void * __ptr32)&length;
+        plist31[2] = (unsigned int)(void * __ptr32)&address; 
         __asm(" SAM31\n"
               " PC  0(%[pc])\n"
               " SAM64\n" 
               : 
               : [pc] "r"(lx[1]),
-                "NR:r1"(plist)
+                "NR:r1"(plist31)
               : "r1", "r15");   
     }      
 }                                                               

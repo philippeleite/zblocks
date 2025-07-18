d5b761ed73440f97c808a09ff30aad7070519bc4
Philippe Leite <philippeleite@192.168.68.104>
2025-07-17 22:07:39 -0300

    App

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="/Users/philippeleite/PycharmProjects/zblocks/app/templates")
 
 from app import routes
